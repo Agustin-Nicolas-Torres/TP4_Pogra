a3228d242cb5a9ada389af1e1362fb14fe126f4f
LeandroBender <benderleandrouniv21@gmail.com>
2025-06-19 20:38:38 -0300

    agregar archivo tecnico

---
+++ Modelos/Tecnico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP4_LEANDRO.Modelos
+{
+    internal class Tecnico
+    {
+        // Atributos de la clase Tecnico
+
+    }
+}
